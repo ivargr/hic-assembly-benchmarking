4fd0deccf46b005631f46be905cc1ee6b59c4115
Ivar Grytten <ivar.grytten@gmail.com>
2023-04-25 11:45:35 +0200

    div

--- install_pbsim.sh
+++ install_pbsim.sh
@@ -1,6 +1,6 @@
 
 set -e
 
-wget -O v3.0.0.zip https://github.com/yukiteruono/pbsim3/archive/refs/tags/v3.0.0.zip
-unzip v3.0.0.zip
+wget -O v3.0.0.tar.gz https://github.com/yukiteruono/pbsim3/archive/refs/tags/v3.0.0.tar.gz
+tar -xvzf v3.0.0.tar.gz
 cd pbsim3-3.0.0 && sudo ./configure && sudo make && sudo make install
